5cf542dd0d89926ccbf5e723fbcf9f4dcdcf2085
Thashreef <thashreefkhan4@gmail.com>
2024-05-09 11:05:29 +0530

    code evolution begining

---
+++ sample/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Greek from './Greek';
+
+function App() {
+  return (
+  
+    <div>
+<Greek/>
+    </div>
+  );
+}
+
+export default App;
---
+++ sample/src/Greek.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+// function type
+// function Greek() {
+//   return (
+//     <div>
+//       <h1>Hello Arshuppa</h1>
+//     </div>
+//   )
+// }
+
+// arrow function type
+const Greek = ()=>{
+   return(
+    
+    <h1>Hello Vishwas</h1>
+    
+    )
+}  
+
+export default Greek
